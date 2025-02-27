df5a201cd3b3b22d11b34c04841bc51995969bf6
mjs227 <92041658+mjs227@users.noreply.github.com>
2025-02-27 15:21:56 +0100

    Add files via upload

---
+++ train_config.py
@@ -0,0 +1,46 @@
+
+import torch
+import model_config as cfg
+
+
+FILEPATH, RUN = '/projects/academic/rchaves/michael_diss/', 0
+CHECKPOINT_FILEPATH = FILEPATH + f'run_{RUN}/checkpoints/'
+OPTIMIZER_FILEPATH = FILEPATH + f'run_{RUN}/optimizers/'
+BASE_DATA_FILEPATH = FILEPATH + 'data/'  # no effect if RANDOMIZE_BATCHES = False
+BATCH_FILEPATH = FILEPATH + f'run_{RUN}/batch_data/'  # empty if RANDOMIZE_BATCHES, else folders {0, ..., N_EPOCHS}
+STATS_FILEPATH = FILEPATH + f'run_{RUN}/stats/'
+LOSS_WEIGHT_FILE = None
+
+MODEL_CONFIG = cfg.SWATForMaskedLMConfig(
+    swat_config__d_model=1024,
+    swat_config__n_encoder_layers=10,
+    swat_config__embedding_config__d_swa=1024,
+    swat_config__embedding_config__n_swa_layers=2
+)
+
+MODEL_TO = {'device': 0}  # do not include device for train_ddp
+TOKEN_DTYPE = None  # None => default
+DDP = False  # False => train.py, True => train_ddp.py
+
+N_EPOCHS = 5
+CHECKPOINT_RATE = 10  # in number of meta-batches (ALWAYS checkpoints at end of epoch)
+CHECKPOINT_ON_ERROR = True  # checkpoint if exception occurs, regardless of checkpoint rate
+CHECKPOINT_ON_KEYBOARD_INTERRUPT = True
+BATCH_SIZE = 16
+COLLECT_BATCHES = False
+RANDOMIZE_BATCHES = True  # True => randomize during training, False => pre-randomized
+PRINT_OUTPUT = False  # print loss (disable for non-debug CCR)
+TQDM = False  # tqdm train loop (disable for ALL CCR) --- no effect if PRINT_OUTPUT = FALSE
+PRINT_RATE = 25  # print loss every PRINT_RATE batches (no effect if PRINT_OUTPUT = false)
+
+# AdamW for weight decay!!!
+OPTIMIZER = torch.optim.AdamW  # torch.optim for train, str for train_ddp
+OPTIMIZER_KWARGS = {'weight_decay': 1e-5}
+INIT_LR = 1e-5  # if len(LR_VALS) == 0, then lr = PEAK_LR
+LR_VALS = [2e-5, 1e-5, 3e-6, 1e-6, 1e-8]  # must be same len as LR_STEP
+LR_STEP = [0.2, 0.4, 0.6, 0.8, 1.0]
+
+SPEC_TOK_P_PROB = 0.0  # special token ([MASK], [PAD], [UNK]) perturb prob.
+P_PROB = 0.2  # non-special token perturb prob.
+M_PROB = 1.0  # mask prob. (of those perturbed)
+S_PROB = 0.0  # swap prob. (of those perturbed)
