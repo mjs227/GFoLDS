a56ae7ab8b90bca4a1198d2817f072ce5f05bf0f
mjs227 <92041658+mjs227@users.noreply.github.com>
2025-04-24 12:51:04 +0200

    Add files via upload

--- experiments/megaveridicality/bert.py
+++ experiments/megaveridicality/bert.py
@@ -1,1 +1,214 @@
 
+import json
+import torch
+import pickle
+from tqdm import tqdm
+from collections import OrderedDict
+from global_filepath import GLOBAL_FP
+from transformers import AutoModelForMaskedLM, AutoTokenizer
+
+
+DATA_FP = GLOBAL_FP + '/factuality/data/mv_data_bin_guc.json'
+PT_FP = GLOBAL_FP + '/bert/runs/run_4/ep3_mb199.chk'
+BERT_MODEL = 'bert-base-uncased'
+RESET_WGTS = False
+PATIENCE = 5
+LR = 1e-6
+WEIGHT_DECAY = 1e-5
+BATCH_SIZE = 8
+DEVICE = 0
+ACTIV_FN = torch.nn.GELU()
+LAYER_NORM = True
+DROPOUT_P = 0.1
+SEED = 25
+
+
+def reset_all_weights(m):
+    @torch.no_grad()
+    def weight_reset(m_):
+        reset_parameters = getattr(m_, 'reset_parameters', None)
+
+        if callable(reset_parameters):
+            m_.reset_parameters()
+
+    m.apply(fn=weight_reset)
+
+
+def pt_sd_to_mlm(in_sd, bert_mlm):
+    mlm_sd_keys, in_sd_keys = map(set, (bert_mlm.state_dict().keys(), in_sd.keys()))
+    assert mlm_sd_keys <= in_sd_keys
+
+    if in_sd_keys == mlm_sd_keys:
+        return in_sd
+
+    new_sd = OrderedDict()
+
+    for k_, v_ in in_sd.items():
+        if k_ in mlm_sd_keys:
+            new_sd.update({k_: v_})
+
+    return new_sd
+
+
+class BERTClassifier(torch.nn.Module):
+    def __init__(self, bert_model, activ_fn=torch.nn.GELU(), layer_norm=True, dropout_p=0.1, reset_wgts=False):
+        super(BERTClassifier, self).__init__()
+        self.bert = AutoModelForMaskedLM.from_pretrained(bert_model).bert
+        d_model = self.bert.embeddings.word_embeddings.weight.size(1)
+
+        if reset_wgts:
+            reset_all_weights(self.bert)
+
+        self.dense = torch.nn.Linear(d_model, d_model)
+        self.activ_fn = self._null_fn if activ_fn is None else activ_fn
+        self.layer_norm = torch.nn.LayerNorm(d_model) if layer_norm else self._null_fn
+        self.dropout = self._null_fn if dropout_p == 0.0 else torch.nn.Dropout(p=dropout_p)
+        self.cls = torch.nn.Linear(d_model, 1)
+
+        self.device = 'cpu'
+        self._save_dict = {
+            'bert_model': bert_model,
+            'activ_fn': activ_fn,
+            'layer_norm': layer_norm,
+            'dropout_p': dropout_p
+        }
+
+    def to(self, *args, **kwargs):
+        super(BERTClassifier, self).to(*args, **kwargs)
+        device = kwargs.get('device', next((a for a in args if not isinstance(a, torch.dtype)), None))
+        self.device = self.device if device is None else device
+
+    def forward(self, x, attn_mask):
+        x = self.bert(x, attention_mask=attn_mask).last_hidden_state
+        x = torch.sum(x * attn_mask[..., None], dim=1) / torch.clamp(attn_mask.sum(dim=1)[..., None], min=1e-9)
+        x = self.dense(x)
+        x = self.activ_fn(x)
+        x = self.layer_norm(x)
+        x = self.dropout(x)
+
+        return self.cls(x)
+
+    def _null_fn(self, x):
+        return x
+
+    @classmethod
+    def from_pretrained(cls, fp, bert_model=None, **kwargs):
+        with open(fp, 'rb') as f_chk:
+            bert_chk = pickle.load(f_chk)
+
+        if 'state_dict' in bert_chk.keys():
+            assert len(kwargs) == 0
+            state_dict_chk = bert_chk.pop('state_dict')
+            cls_out = cls(**bert_chk)
+            cls_out.load_state_dict(state_dict_chk)
+        else:
+            assert bert_model is not None
+            cls_out = cls(bert_model, **kwargs)
+            bert = AutoModelForMaskedLM.from_pretrained(bert_model)
+            bert.load_state_dict(pt_sd_to_mlm(bert_chk, bert))
+            del_bert = cls_out.bert
+            cls_out.bert = bert.bert
+            del del_bert
+
+        return cls_out
+
+
+def generate_data(data_list):
+    long_tensor = torch.LongTensor if DEVICE == 'cpu' else torch.cuda.LongTensor
+
+    for k in range(-(-len(data_list) // BATCH_SIZE)):
+        batch_data = data_list[k * BATCH_SIZE:(k + 1) * BATCH_SIZE]
+        toks = tokenizer([x['s'] for x in batch_data], padding=True)
+        toks_ten = torch.tensor(toks['input_ids'], device=DEVICE)[:, 1:-1]
+        mask_ten = torch.tensor(toks['attention_mask'], device=DEVICE)[:, 1:-1]
+        trgt_ten = torch.tensor([x['l'] for x in batch_data], device=DEVICE).type(long_tensor)
+
+        yield toks_ten, mask_ten, trgt_ten
+
+
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+
+tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL)
+
+if PT_FP is None:
+    model = BERTClassifier(
+        BERT_MODEL,
+        activ_fn=ACTIV_FN,
+        reset_wgts=RESET_WGTS,
+        layer_norm=LAYER_NORM,
+        dropout_p=DROPOUT_P
+    )
+else:
+    model = BERTClassifier.from_pretrained(
+        PT_FP,
+        bert_model=BERT_MODEL,
+        activ_fn=ACTIV_FN,
+        reset_wgts=RESET_WGTS,
+        layer_norm=LAYER_NORM,
+        dropout_p=DROPOUT_P
+    )
+
+model.to(device=DEVICE)
+model.train()
+optimizer = torch.optim.AdamW(model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
+
+with open(DATA_FP, 'r') as f:
+    data_file = json.load(f)
+
+train_data, test_data = data_file['train'], data_file['test']
+ep_cnt, patience_cnt, best_acc = 0, 0, 0.0
+loss_fn = torch.nn.BCEWithLogitsLoss()
+
+while patience_cnt < PATIENCE:
+    ep_cnt += 1
+    print(f'EPOCH {ep_cnt}:')
+    print()
+
+    optimizer.zero_grad()
+    permutation = torch.randperm(len(train_data)).tolist()
+    train_batches = generate_data(list(map(lambda i_: train_data[i_], permutation)))
+    epoch_loss, loss_cnt = 0.0, 0
+
+    for _ in tqdm(range(-(-len(train_data) // BATCH_SIZE))):
+        batch_ids, batch_mask, batch_trgts = next(train_batches)
+        model_out = model(batch_ids, batch_mask)
+        loss = loss_fn(model_out.flatten(), batch_trgts.float())
+
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+        loss_cnt += batch_ids.size(0)
+        loss_cnt += 1
+
+        optimizer.zero_grad()
+        del batch_ids, batch_mask, batch_trgts, model_out, loss
+
+    print()
+    print(f'LOSS: {epoch_loss / loss_cnt}')
+    print()
+
+    test_batches = generate_data(test_data)
+    epoch_acc, acc_cnt = 0.0, 0
+    model.eval()
+
+    with torch.no_grad():
+        for _ in tqdm(range(-(-len(test_data) // BATCH_SIZE))):
+            test_ids, test_mask, test_trgts = next(test_batches)
+            model_out = model(test_ids, test_mask)
+
+            epoch_acc += torch.sum((model_out.flatten() >= 0.0) == test_trgts.flatten()).item()
+            acc_cnt += test_ids.size(0)
+
+            del test_ids, test_mask, test_trgts, model_out
+
+    acc = epoch_acc / acc_cnt
+
+    if acc > best_acc:
+        best_acc, patience_cnt = acc, 0
+    else:
+        patience_cnt += 1
+
+    print()
+    print(f'ACC: {round(acc * 100, 5)}% (BEST={round(best_acc * 100, 5)}%; PATIENCE={patience_cnt})')
+    print()
---
+++ experiments/megaveridicality/generate_data.py
@@ -0,0 +1,138 @@
+
+import os
+import re
+import json
+import random
+from tqdm import tqdm
+from subprocess import DEVNULL
+from global_filepath import GLOBAL_FP
+from model.graphtools import GraphParser, SWADirGraph
+from model import PROHIBITED_TOKENS, LINK_ROLE_MAP, GRPH_LABEL_IDX
+
+
+DATA_FP = GLOBAL_FP + '/factuality/data/mega-veridicality-v2.1.tsv'
+OUT_FP = GLOBAL_FP + '/factuality/mv_data_bin_guc.json'
+PARSER_FP = GLOBAL_FP + '/tokenizer_config.json'
+BIN = True
+GUC = True
+
+
+class MVGraphParser(GraphParser):
+    def __init__(self, grammar, tokenizer_dict, unk_as_mask=True, _fpt=False):
+        super(MVGraphParser, self).__init__(_fpt=_fpt)
+        from delphin import ace, dmrs
+
+        self.grammar, self.tokenizer_dict = os.path.abspath(grammar), tokenizer_dict
+        ace_fp = self.grammar[:-len(self.grammar.split('/')[-1])]
+
+        if re.search(re.compile(f'(^|:){re.escape(ace_fp)}($|:)'), os.environ['PATH'].strip()) is None:
+            os.environ['PATH'] += ('' if len(os.environ['PATH'].strip()) == 0 else ':') + ace_fp
+
+        self.mrs_parser, self._to_dmrs = ace, dmrs.from_mrs
+        self._unk_toks, self.unk_as_mask = {}, unk_as_mask
+        assert set(tokenizer_dict.keys()) == {'v', 'e', 'f'}
+
+        for k_ in ('v', 'e', 'f'):
+            if unk_as_mask or '[UNK]' not in self.tokenizer_dict[k_].keys():
+                self._unk_toks.update({k_: self.tokenizer_dict[k_].get('[MASK]', -1)})
+            else:
+                self._unk_toks.update({k_: self.tokenizer_dict[k_]['[UNK]']})
+
+        self._num_sg = self.tokenizer_dict['f']['[NUM:sg]']
+        self._num_pl = self.tokenizer_dict['f']['[NUM:pl]']
+
+    def save(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _call(self, input_sentences: str, **_):
+        for line_ in map(lambda s: s.strip(), input_sentences):
+            if len(line_) > 0:
+                yield self._parse_template(line_)
+
+    def _parse_template(self, in_sent):
+        response = next(self.mrs_parser.parse_from_iterable(self.grammar, (in_sent,), cmdargs=['-1'], stderr=DEVNULL))
+
+        if len(response['results']) == 0:
+            return {'g': None}
+
+        dmrs = self._to_dmrs(response.result(0).mrs())
+        graph, pred_id_dict, no_unk, no_carg = SWADirGraph(), {}, True, True
+
+        for pred in dmrs.predications:
+            no_carg = no_carg and pred.carg is None
+            pred_name = pred.predicate.lower()
+            pred_name = pred_name[1:] if pred_name[0] == '_' else pred_name
+
+            if pred_name not in PROHIBITED_TOKENS:  # {'focus_d', 'parg_d'}
+                if '/' in pred_name:  # OOV
+                    pred_id = graph.add_node(self._unk_toks['v'])
+                else:
+                    pred_id = graph.add_node(self._tokenize_fn(pred_name, 'v'))
+
+                for feat, val in pred.properties.items():
+                    graph.add_feature(pred_id, self._tokenize_fn(f'[{feat}:{val}]', 'f'))
+
+                pred_id_dict.update({pred.id: pred_id})
+                no_unk = no_unk and not graph.nodes[pred_id][GRPH_LABEL_IDX] == self._unk_toks['v']
+
+        for link in dmrs.links:
+            if link.start in pred_id_dict.keys() and link.end in pred_id_dict.keys():
+                graph.add_edge(
+                    pred_id_dict[link.start],
+                    pred_id_dict[link.end],
+                    self._tokenize_fn(LINK_ROLE_MAP.get(link.role, link.role), 'e')
+                )
+
+        return {'g': graph.save(), 's': in_sent, 'no_unk': no_unk, 'no_carg': no_carg}
+
+    def _tokenize_fn(self, s, kind):
+        return self.tokenizer_dict[kind].get(s, self._unk_toks[kind])
+
+    @classmethod
+    def init_for_pretraining(cls, *args, **kwargs) -> None:
+        raise NotImplementedError
+
+    @classmethod
+    def from_pretrained(cls, filepath, **kwargs) -> "MVGraphParser":
+        with open(os.path.abspath(filepath), 'r') as f_pt:
+            fp_kwargs = {**json.load(f_pt), **kwargs, **{'_fpt': True}}
+
+        return cls(fp_kwargs.pop('grammar'), fp_kwargs.pop('tokenizer_dict'), **fp_kwargs)
+
+
+parser = MVGraphParser.from_pretrained(PARSER_FP)
+label_map = {'yes': 1, 'maybe': 0, 'no': -1}
+data_dict, out_data = {}, []
+
+with open(DATA_FP, 'r') as f:
+    for line in map(lambda z: z.strip(), f):
+        if len(line) > 0 and not line.startswith('participant'):
+            line_items = line.split('\t')
+            label = line_items[8].strip()
+
+            if len(label) > 0:
+                sent = line_items[7].strip().replace('\\\'', '\'')
+
+                if sent in data_dict.keys():
+                    data_dict[sent].append(label_map[label])
+                else:
+                    data_dict.update({sent: [label_map[label]]})
+
+for k, v in tqdm(data_dict.items()):
+    grph = parser(k, save=True, include_text=True)
+
+    if (grph['g'] is not None) and ((grph['no_unk'] and grph['no_carg']) or not GUC):
+        grph.update({'l': (lambda w: (1 if w > 0 else 0) if BIN else w)(sum(v) / len(v))})
+        out_data.append(grph)
+
+random.shuffle(out_data)
+len_train = len(out_data) * 4 // 5
+out_file = {'train': out_data[:len_train], 'test': out_data[len_train:]}
+
+with open(OUT_FP, 'w') as f:
+    json.dump(out_file, f)
+
+print()
+print(f'PARSE RATE: {len(out_data) * 100 / len(data_dict.keys())}%')
+print(f'TRAIN: {len(out_file["train"])}')
+print(f'TEST:  {len(out_file["test"])}')
