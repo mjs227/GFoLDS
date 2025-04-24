19617291956d6fcdac3e27bb022af49fd7d2c35d
mjs227 <92041658+mjs227@users.noreply.github.com>
2025-04-24 12:43:43 +0200

    Add files via upload

--- model/io/__init__.py
+++ model/io/__init__.py
@@ -1,1 +1,11 @@
 
+from model.io._io import (
+    SWATInput,
+    SWATForMLMInput,
+    SWATForTCInput,
+    SWATForSCInput,
+    SWATOutput,
+    SWATForMLMOutput,
+    SWATForTCOutput,
+    SWATForSCOutput
+)
