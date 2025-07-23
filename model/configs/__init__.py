3fc881ce990a147626337fc4d39b938f1ac1d653
mjs227 <92041658+mjs227@users.noreply.github.com>
2025-07-23 16:21:43 +0200

    Add files via upload

--- model/configs/__init__.py
+++ model/configs/__init__.py
@@ -1,1 +1,15 @@
 
+from model.configs._configs import (
+    SWATWithClsHeadConfig,
+    SWATForMaskedLMConfig,
+    SWATForSequenceClassificationConfig,
+    SWATForTokenClassificationConfig,
+    SWATConfig,
+    SWATEncoderConfig,
+    SWATFeedForwardConfig,
+    SWATDecoderConfig,
+    SWATEmbeddingConfig,
+    MaskDropoutConfig,
+    SWALayerConfig,
+    SWAModuleConfig
+)
