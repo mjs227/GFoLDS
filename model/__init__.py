c23ba0f7377ecc3ff24cd5a105cafac4ee17fb43
mjs227 <92041658+mjs227@users.noreply.github.com>
2025-07-23 16:21:00 +0200

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,22 @@
+
+from model._constants import ID as GRPH_ID_IDX
+from model._constants import IDX as GRPH_IDX_IDX
+from model._constants import LABEL as GRPH_LABEL_IDX
+from model._constants import VOCAB_SIZE as DEFAULT_VOCAB_SIZE
+from model._constants import FEATURE_SIZE as DEFAULT_FEATURE_SIZE
+from model._constants import (
+    MAX_SEQ_LEN,
+    SPEC_TOK_RNG,
+    PAD_TOKEN_ID,
+    MASK_TOKEN_ID,
+    IGNORE_ID,
+    DEFAULT_D_MODEL,
+    DEFAULT_D_SWA,
+    DEFAULT_N_ENC,
+    DEFAULT_N_SWA,
+    DEFAULT_N_HEADS,
+    DEFAULT_P_DROPOUT,
+    PROHIBITED_TOKENS,
+    LINK_ROLE_MAP,
+    DEFAULT_TOKENIZER_DICT
+)
